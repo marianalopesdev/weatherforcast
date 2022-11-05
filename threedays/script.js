154cff72a83dfc765796fc49dc6ff0368f617f57
marianalopesdev <52184708+marianalopesdev@users.noreply.github.com>
2022-11-05 03:27:30 -0300

    function to check the month name added

--- threedays/script.js
+++ threedays/script.js
@@ -6,7 +6,7 @@ function searchWeather() {
             'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
         }
     };
-    
+
     const input = document.getElementById("searchInput").value;
 
     fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + input + '&days=3', options)
@@ -27,24 +27,54 @@ function retrievingData(response) {
     weatherNow.setAttribute('src', currentConditionIcon);
     weatherNowTitle.innerText = response.current.condition.text;
 
-
     populateNextDaysForecast(response);
-   
+
 
 }
 
 function populateNextDaysForecast(response = []) {
-
-  //  const taskList = document.getElementById('taskList');
     const nextdaysForecast = response.forecast.forecastday;
     nextdaysForecast.shift();
     nextdaysForecastList.innerHTML = nextdaysForecast.map((forecast) => {
         const day = forecast.date.substr(8, 2);
-        const month = forecast.date.substr(5, 2);
+        let month = forecast.date.substr(5, 2);
+        let monthName = checkMonthName(month);
+        let maxTempCelsius = forecast.day.maxtemp_c;
+        let maxTempFarenheit = forecast.day.maxtemp_f;
         return `
-       <label>Data: ${day}/${month}</label><br>
-       <label>Temperatura máxima: ${forecast.day.maxtemp_c}</label><br>
+       <label>Data: ${monthName}, ${day}</label><br>
+       <label>Temperatura máxima (Celsius): ${maxTempCelsius}</label><br>
+       <label>Temperatura máxima (Farenheit): ${maxTempFarenheit}</label><br>
        <label>Temperatura mínima: ${forecast.day.mintemp_c}</label><br><br>
             `;
     }).join('');
+}
+
+function checkMonthName(month) {
+    switch (month) {
+        case '01':
+            return month = 'January';
+        case '02':
+            return month = 'February';
+        case '03':
+            return month = 'March';
+        case '04':
+            return month = 'April';
+        case '05':
+            return month = 'May';
+        case '06':
+            return month = 'June';
+        case '07':
+            return month = 'July';
+        case '08':
+            return month = 'August';
+        case '09':
+            return month = 'September';
+        case '10':
+            return month = 'October';
+        case '11':
+            return month = 'November';
+        case '12':
+            return month = 'December';
+    }
 }
